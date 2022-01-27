7c080fb68f94cafb605a89e3ee9775160ccf7c57
long vu duc <vuduclong10a1cma@gmail.com>
2022-01-27 12:02:24 +0700

    add some code

--- ex1/plotData.m
+++ ex1/plotData.m
@@ -7,7 +7,7 @@ figure; % open a new figure window
 data = load('ex1data1.txt')
 x = data(:,1);
 y = data(:,2);
-plot(x,y,'rx','MarkerSize', 10)
+plot(x,y,'rx')
 xlabel('population')
 ylabel('revenue')
 % ====================== YOUR CODE HERE ======================
